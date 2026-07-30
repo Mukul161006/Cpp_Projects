ccb5f3226f984d5c543ef5daf1a3eec75959fac6
Mukul161006 <mukulpratapsingh1610@gmail.com>
2026-07-31 00:24:46 +0530

    Function for Student table and errorMessage functioning

--- LMS.cpp
+++ LMS.cpp
@@ -12,15 +12,42 @@ struct Books {
 
 };
 
-void createTables( sqlite3* db) {
+int createTables( sqlite3* db) {
 
     // Ye jo hai koi error aayi toh show kr dega. 
     char* errorMessage = 0; 
 
+    //=================== FOR THE BOOKS TABLE ===================// 
+    // Books naam ki table ka blueprint pass krne k liye SQL Command. 
     const char* sqlBooks = "CREATE TABLE IF NOT EXISTS books ("
                            "book_id INTEGER PRIMARY KEY, "
                            "name TEXT NOT NULL, "
                            "author TEXT NOT NULL);";
+
+    // Execute krna ka command taaki vo SQL Queries actually run ho. 
+    int engine_reply = sqlite3_exec(db, sqlBooks, 0, 0, &errorMessage);
+
+    if(engine_reply != SQLITE_OK){
+        cout << "SOME ISSUE HAPPENED BHAISABH " << errorMessage << endl; 
+        return 1; 
+    } else {
+        cout << "SUCK....SEX..FULLY CREATED THE TABLE books --------------- " << endl;
+    }
+
+    //=================== NOW FOR THE STUDENTS TABLE ===================// 
+    const char* sqlStudents = "CREATE TABLE IF NOT EXISTS students ("
+                           "roll_no INTEGER PRIMARY KEY, "
+                           "name TEXT NOT NULL, "
+                           "course TEXT NOT NULL);";
+
+    int engine_reply1 = sqlite3_exec(db, sqlStudents, 0, 0, &errorMessage);
+
+    if(engine_reply1 != SQLITE_OK){
+        cout << "SOME ISSUE HAPPENED BHAISABH " << errorMessage << endl; 
+        return 1; 
+    } else {
+        cout << "SUCK....SEX..FULLY CREATED THE TABLE students --------------- " << endl;
+    }
 
 }
 
@@ -36,6 +63,9 @@ int main (){
     } else {
         cout << "File hai re baba aur open bhi ho rhi hai" << endl;
     }
+
+    // Calling table creation function to pass the memory address at which the table should be created re bhai. 
+    createTables(db);
 
     sqlite3_close(db);
 
