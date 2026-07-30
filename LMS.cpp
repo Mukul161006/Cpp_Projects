10d99ee85785f1d1df93a7e2c75756c685d77a30
Mukul161006 <mukulpratapsingh1610@gmail.com>
2026-07-31 01:42:05 +0530

    data feeding of stud and book from user

--- LMS.cpp
+++ LMS.cpp
@@ -6,11 +6,18 @@ using namespace std;
 
 struct Books {
 
-    int book_id; 
+    string book_id; 
     string name; 
     string author;
-
 };
+
+struct Students {
+    
+    string roll_no; 
+    string name; 
+    string course;
+};
+
 
 int createTables( sqlite3* db) {
 
@@ -20,7 +27,7 @@ int createTables( sqlite3* db) {
     //=================== FOR THE BOOKS TABLE ===================// 
     // Books naam ki table ka blueprint pass krne k liye SQL Command. 
     const char* sqlBooks = "CREATE TABLE IF NOT EXISTS books ("
-                           "book_id INTEGER PRIMARY KEY, "
+                           "book_id TEXT PRIMARY KEY, "
                            "name TEXT NOT NULL, "
                            "author TEXT NOT NULL);";
 
@@ -36,7 +43,7 @@ int createTables( sqlite3* db) {
 
     //=================== NOW FOR THE STUDENTS TABLE ===================// 
     const char* sqlStudents = "CREATE TABLE IF NOT EXISTS students ("
-                           "roll_no INTEGER PRIMARY KEY, "
+                           "roll_no TEXT PRIMARY KEY, "
                            "name TEXT NOT NULL, "
                            "course TEXT NOT NULL);";
 
@@ -48,8 +55,44 @@ int createTables( sqlite3* db) {
     } else {
         cout << "SUCK....SEX..FULLY CREATED THE TABLE students --------------- " << endl;
     }
-
 }
+
+
+// Function for taking vlaues from the user about the Book. 
+int addBook (sqlite3* db, Books newBook){
+
+    char* errorMessage = 0; 
+
+    string sqlInsertBook = "INSERT INTO books (book_id, name, author) VALUES ('" + newBook.book_id + "', '" + newBook.name + "', '" + newBook.author + "');";
+
+    int status = sqlite3_exec(db, sqlInsertBook.c_str(), 0, 0, &errorMessage);
+
+    if ( status != SQLITE_OK){
+        cout << "$$$$$$$ SOMETHING IS WRONG $$$$$$$ : " << errorMessage << endl;
+        return 1; 
+    } else {
+        cout << "THE DATA GOT ADDED SIRRRRRRR !!" << endl;
+    }
+}
+
+
+// Function for taking vlaues from the user about the student. 
+int addStudent (sqlite3* db, Students newStud){
+
+    char* errorMessage = 0; 
+
+    string sqlInsertStud = "INSERT INTO students (roll_no, name, course) VALUES ('" + newStud.roll_no + "', '" + newStud.name + "', '" + newStud.course + "');";
+
+    int status = sqlite3_exec(db, sqlInsertStud.c_str(), 0, 0, &errorMessage);
+
+    if ( status != SQLITE_OK){
+        cout << "$$$$$$$ SOMETHING IS WRONG $$$$$$$ : " << errorMessage << endl;
+        return 1; 
+    } else {
+        cout << "THE DATA GOT ADDED SIRRRRRRR !!" << endl;
+    }
+}
+
 
 int main (){
 
@@ -66,6 +109,27 @@ int main (){
 
     // Calling table creation function to pass the memory address at which the table should be created re bhai. 
     createTables(db);
+
+    Books userBook; 
+    cout << "\n---- ADD A NEW BOOK ----" << endl;
+    cout << "Enter your BOOK ID \t"; 
+    getline(cin >> ws, userBook.book_id);
+    cout << "Enter BOOK NAME\t";
+    getline(cin >> ws, userBook.name);
+    cout << "Enter AUTHOR'S NAME\t";
+    getline(cin >> ws, userBook.author);
+    addBook(db, userBook);
+
+    Students userStud; 
+    cout << "\n---- ADD A NEW STUDENT ----" << endl;
+    cout << "Enter Student's Roll Number \t"; 
+    getline(cin >> ws, userStud.roll_no);
+    cout << "Enter STUDENT NAME\t";
+    getline(cin >> ws, userStud.name);
+    cout << "Enter STUDENT'S COURSE\t";
+    getline(cin >> ws, userStud.course);
+    addStudent(db, userStud);
+
 
     sqlite3_close(db);
 
