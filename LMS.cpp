5f7873cd00b06120f48e6b35011756f6f2134bbc
Mukul161006 <mukulpratapsingh1610@gmail.com>
2026-07-30 20:16:12 +0530

    Saving my local struct code before merging

--- LMS.cpp
+++ LMS.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <sqlite3.h>
-
 using namespace std;
+#include "sqlite3.h"
+#include <string>
 
-struct Books{
+struct Books {
 
+    string title; 
+    string author; 
 
 };
