e8ea4e3dadbebc6292fbeddfff369ea5c8fa123f
Mukul161006 <mukulpratapsingh1610@gmail.com>
2026-08-23 23:00:03 +0530

    another program

--- cpp_exam_prep/sort_selection.cpp
+++ cpp_exam_prep/sort_selection.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
 using namespace std;
 
+void selectionSort(int arr[], int n){
+
+    for(int mainPos = 0; mainPos < n-1; mainPos++){
+
+        int minIndex = mainPos; 
+        
+        for(int scanner = mainPos + 1; scanner < n; scanner++){
+            if(arr[scanner] < arr[minIndex]) {
+                minIndex = scanner;
+            }
+        }
+
+        if(minIndex != mainPos){
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[mainPos]; 
+            arr[mainPos] = temp; 
+        }
+    }
+
+}
+
+
 int main(){
 
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
 
+    selectionSort(arr, n);
+
+    cout << "Selection Sorted Array: ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    
     return 0; 
 }
