2626f65586bceaea4056d2bb127ef15a903bf083
Mukul161006 <mukulpratapsingh1610@gmail.com>
2026-08-23 19:47:37 +0530

    binary search also done.

--- cpp_exam_prep/BinarySeach.cpp
+++ cpp_exam_prep/BinarySeach.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main(){
 
     int arr[10] = {1,2,3,4,5,6,7,8,9,10};
-    int target; 
+    int target;
+    bool found = false;  
 
     // here we specify the index of the array.
     int low = 0;  
@@ -13,10 +14,27 @@ int main(){
     cout << "Target ?? "; 
     cin >> target;
 
+    while(low <= high){
+
+        // Find the exact middle. 
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == target){
+            cout << "Target value " << target << " found at this index position -> " << mid + 1 << endl; 
+            found = true;
+            break;
+        } 
+        else if (arr[mid] < target){
+            low = mid + 1; 
+        }
+        else { //(arr[mid] > target) wala case. 
+            high = mid - 1; 
+        }
+    }
     
-
-
-
+    if (!found) {
+        cout << "Target not found !!" << endl;
+    }
 
     return 0; 
 }
